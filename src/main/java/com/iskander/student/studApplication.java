f2ac8557e4e89fdacca7108f4f27ad2af9e61dfd
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-10-19 18:34:08 +0500

    setup

---
+++ src/main/java/com/iskander/student/studApplication.java
@@ -0,0 +1,14 @@
+package com.iskander.student;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class studApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(studApplication.class, args);
+    }
+
+}
